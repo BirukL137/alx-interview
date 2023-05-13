3ece5cd99b921ccbb71a4b2b7f4794da749d2eb7
BirukL137 <buraf13k@gmail.com>
2023-05-13 13:36:02 +0300

    0. Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,39 +6,43 @@ A script that reads stdin line by line and computes metrics.
 import sys
 
 
-def display_status(total_size, status_codes):
-    """
-    Displays the sorted status.
-    """
-    print(f"File size: {total_size}")
-    for k, v in sorted(status_codes.items()):
-        print(f"{k}: {v}")
-
-
 if __name__ == "__main__":
-    count = 0
-    total_size = 0
-    status_codes = {}
+    def display(total_size, status_codes):
+        """
+        A method that displays sorted status.
+        """
+        print(f"File size: {total_size}")
+        for k, v in sorted(status_codes.items()):
+            print(f"{k}: {v}")
 
-    codes = {'200', '301', '400', '401', '402', '403', '404', '405', '500'}
+    def parse_status():
+        """
+        A method that reads and take data.
+        """
+        count = 0
+        total_size = 0
+        status_codes = {}
 
-    try:
-        for line in sys.stdin:
-            count += 1
-            line = line.split()
-            try:
-                total_size += int(line[-1])
-                if line[-2] in codes:
-                    try:
-                        status_codes[line[-2]] += 1
-                    except KeyError:
-                        status_codes[line[-2]] = 1
-            except (IndexError, ValueError):
-                pass
-            if count == 10:
-                display_status(total_size, status_codes)
-                count = 0
-        display_status(total_size, status_codes)
-    except KeyboardInterrupt as e:
-        display_status(total_size, status_codes)
-        raise
+        codes = {'200', '301', '400', '401', '402', '403', '404', '405', '500'}
+
+        try:
+            for line in sys.stdin:
+                count += 1
+                line = line.split()
+                try:
+                    total_size += int(line[-1])
+                    if line[-2] in codes:
+                        try:
+                            status_codes[line[-2]] += 1
+                        except KeyError:
+                            status_codes[line[-2]] = 1
+                except (IndexError, ValueError):
+                    pass
+                if count == 10:
+                    display(total_size, status_codes)
+                    count = 0
+            display(total_size, status_codes)
+        except KeyboardInterrupt as e:
+            display(total_size, status_codes)
+            raise
+    parse_status()
