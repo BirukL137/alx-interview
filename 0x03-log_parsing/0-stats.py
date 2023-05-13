f4f6a71179fe30fd022efbe83ee594a6d2d029e8
BirukL137 <buraf13k@gmail.com>
2023-05-13 14:25:58 +0300

    0. Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -15,36 +15,37 @@ def display(total_size, status_codes):
         print(f"{k}: {v}")
 
 
+def parse_status():
+    """
+    A method that reads and take data.
+    """
+    count = 0
+    total_size = 0
+    status_codes = {}
+
+    codes = {'200', '301', '400', '401', '402', '403', '404', '405', '500'}
+
+    try:
+        for line in sys.stdin:
+            count += 1
+            line = line.split()
+            try:
+                total_size += int(line[-1])
+                if line[-2] in codes:
+                    try:
+                        status_codes[line[-2]] += 1
+                    except KeyError:
+                        status_codes[line[-2]] = 1
+            except (IndexError, ValueError):
+                pass
+            if count == 10:
+                display(total_size, status_codes)
+                count = 0
+        display(total_size, status_codes)
+    except KeyboardInterrupt as e:
+        display(total_size, status_codes)
+        raise
+
+
 if __name__ == "__main__":
-
-    def parse_status():
-        """
-        A method that reads and take data.
-        """
-        count = 0
-        total_size = 0
-        status_codes = {}
-
-        codes = {'200', '301', '400', '401', '402', '403', '404', '405', '500'}
-
-        try:
-            for line in sys.stdin:
-                count += 1
-                line = line.split()
-                try:
-                    total_size += int(line[-1])
-                    if line[-2] in codes:
-                        try:
-                            status_codes[line[-2]] += 1
-                        except KeyError:
-                            status_codes[line[-2]] = 1
-                except (IndexError, ValueError):
-                    pass
-                if count == 10:
-                    display(total_size, status_codes)
-                    count = 0
-            display(total_size, status_codes)
-        except KeyboardInterrupt as e:
-            display(total_size, status_codes)
-            raise
     parse_status()
